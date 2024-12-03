38b94ca753b8dcc472947ab216d7dd223a19ea6d
Maximilien Servajean <maximiliense@gmail.com>
2024-12-03 09:02:05 +0100

    list corrected

---
+++ lib/widgets/list/util/item_type.dart
@@ -0,0 +1,11 @@
+import 'package:decodart/model/abstract_item.dart' show AbstractListItem;
+
+ItemType defaultItemTypeFct(AbstractListItem item) {
+  return ItemType.artwork;
+}
+
+enum ItemType {
+  museum,
+  artwork,
+  tour
+}
